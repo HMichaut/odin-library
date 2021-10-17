561a000bec3ceed0de252086438f3f022ecbf4cf
HMichaut <hectormichaut@gmail.com>
2021-10-17 13:22:28 +0200

    create table function finalized

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function Book(title, author, pages, read) {
   this.author = author
   this.pages = pages
   this.read = read
-  this.info = function() {
-    let status = read ? "already read" : "not read yet";
-    return title + " by " + author + ", " + pages + "pages, " + status;
-  }
+  // this.info = function() {
+  //   let status = read ? "already read" : "not read yet";
+  //   return title + " by " + author + ", " + pages + "pages, " + status;
+  // }
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -21,7 +21,37 @@ function logAllBooksInLibrary() {
   }
 }
 
+function insertAllBooksInLibraryInTable() {
+  const body = document.body,
+        tbl = document.createElement('table');
+  tbl.style.width = '100px';
+  tbl.style.border = '1px solid black';
+
+  let orderArrayHeader = ["Title","Author","Pages","Read"];
+  let thead = document.createElement('thead');
+  tbl.appendChild(thead);
+  
+  for(var i=0;i<orderArrayHeader.length;i++){
+    const th = thead.appendChild(document.createElement("th"));
+    th.appendChild(document.createTextNode(orderArrayHeader[i]));
+    th.style.border = '1px solid black';
+  }
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const tr = tbl.insertRow();
+    console.log(i)
+    Object.keys(myLibrary[i]).forEach(function(key, j) {
+      const td = tr.insertCell();
+      td.appendChild(document.createTextNode(myLibrary[i][key]));
+      td.style.border = '1px solid black';
+    });
+  }
+  body.appendChild(tbl);
+}
+
+
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false)
 addBookToLibrary('test title', 'test author', 15, true)
 
-logAllBooksInLibrary()
+// logAllBooksInLibrary()
+insertAllBooksInLibraryInTable()
